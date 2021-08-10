883fe19174abb96aa527be4ec4a9fe9654dd444e
Sarthak Dhakal <sarthakdhakal03@gmail.com>
2021-08-10 11:36:58 +0545

    Initial commit

---
+++ Practice2/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Practice2.Models;
+
+namespace Practice2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+         PracticeDbEntities1 _db = new PracticeDbEntities1();
+        // GET
+        public ActionResult Index()
+        {
+            List<Employee> empList = _db.Employees.ToList();
+            List<EmployeeDataModel> employeeDataModels = empList.Select(x => new EmployeeDataModel()
+            {
+                Name = x.Name,
+                Address = x.Address,
+                EmployeeId = x.EmployeeId,
+            
+                DepartmentName = x.Department.DepartmentName
+                
+            }).ToList();
+            return View(employeeDataModels);
+        }
+
+        public ActionResult Create()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ActionResult EmployeeDetails()
+        {
+            throw new System.NotImplementedException();
+        }
+    
+        public ActionResult AddEdit(int employeeId)
+        {
+            return PartialView();  
+        }
+        [HttpPost]
+        public ActionResult AddEdit(Employee employee)
+        {   
+            
+            return View();
+        }
+    }
+}
---
+++ Practice2/Models/EmployeeDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice2.Models
+{
+    public class EmployeeDataModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> DepartmentId { get; set; }
+        public string Address { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+    
+        public virtual Department Department { get; set; }
+        
+        public string DepartmentName   {get; set; }
+    }
+}
